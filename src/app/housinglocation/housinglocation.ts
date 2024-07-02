0dea98db564c167c54ce9535f35da57da1db1eb0
Ahmed8881 <buttahmed2004@gmail.com>
2024-07-02 12:57:34 +0500

    style: Update housing location component CSS

---
+++ src/app/housinglocation/housinglocation.ts
@@ -0,0 +1,10 @@
+export interface Housinglocation {
+    id:number;
+    name:string;
+    city:string;
+    state:string;
+    photo:string;
+    availableUnits:number;
+    wifi:boolean;
+    laundry:boolean;
+}
