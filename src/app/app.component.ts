a3235a1ff8cfc264e9c42086a62e1d4d2f913458
Ahmed8881 <buttahmed2004@gmail.com>
2024-07-02 11:29:58 +0500

    chore: Update project configuration and templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,10 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  template: `<h1>Default</h1>
+  <p>cl</p> `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularProject';
+  title = 'homes';
 }
