a313033a2ee7468aec0b0a90c4338a63c8c9bee3
Ahmed8881 <buttahmed2004@gmail.com>
2024-07-02 14:39:35 +0500

    feat: Add lazy loading for images in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
 import { HousinglocationComponent } from '../housinglocation/housinglocation.component';
+import { Housinglocation } from '../housinglocation/housinglocation';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [HousinglocationComponent],
+
   template: ` 
   
   <section>
@@ -20,5 +23,16 @@ import { HousinglocationComponent } from '../housinglocation/housinglocation.com
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+  housinglocations:Housinglocation={
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 
 }
